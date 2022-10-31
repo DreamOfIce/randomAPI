7d009835368b40425f6d27cfdad5e584ca2f0d73
DreamOfIce <dreamofice@outlook.com>
2022-10-31 23:34:04 +0800

    Complete hitokoto

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { config, logger } from './lib/globals.js';
-import { processUpdateRequest, update } from './lib/update.js';
+import { handleUpdateRequest, update } from './lib/update.js';
 import hitokoto from './lib/hitokoto.js';
 import media from './lib/media.js';
+import parser from './lib/parser.js';
 
 const app = express();
 
@@ -10,25 +11,45 @@ const app = express();
 config.apis.forEach((api) => {
   switch (api.type) {
     case 'media':
-      app.use(api.path, media);
+      app.use(
+        api.path,
+        express.json(),
+        express.urlencoded({ extended: true }),
+        parser,
+        media,
+      );
       break;
     case 'hitokoto':
-      app.use(api.path, hitokoto);
+      app.use(
+        api.path,
+        express.json(),
+        express.urlencoded({ extended: true }),
+        parser,
+        hitokoto,
+      );
       break;
     default:
       throw new Error(`Invalid API type:${api.type}!`);
   }
 });
-app.use('/update', processUpdateRequest);
+app.use(
+  '/update',
+  express.raw({
+    limit: '10mb',
+    type: ['application/json', 'application/x-www-form-urlencoded'],
+  }),
+  handleUpdateRequest,
+);
 
-// Get resources list
-logger.info('Start getting resources...');
-update();
-logger.info('Done.');
-
-// Start server
-const host = process.env.HOST || '0.0.0.0';
-const port = process.env.PORT || 8006;
-app.listen(port, host, () => {
-  logger.info(`Start listening on http://${host}:${port}`);
-});
+(async () => {
+  // Get resources list
+  logger.info('Start getting resources...');
+  await update();
+  logger.info('Done.');
+  // Start server
+  const host = process.env.HOST || '0.0.0.0';
+  const port = process.env.PORT || 8006;
+  app.listen(port, host, () => {
+    logger.info(`Start listening on http://${host}:${port}`);
+  });
+})();
